a8de01f7c5c88c6018b887455e7201510d9ea696
yang <yrzhang@thoughtworks.com>
2020-05-22 13:56:34 +0800

    clear build script' :

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,6 @@ set -eu
 set -o pipefail
 
 # build
-nuget locals http-cache -clear
-dotnet restore ./Swashbuckle.AspNetCore.ExampleBuilder.sln
 dotnet build ./Swashbuckle.AspNetCore.ExampleBuilder.sln
 
 # ut
